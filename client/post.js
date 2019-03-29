75eab755bfdda0126b4e9d9df1b4a01c79b5f54f
stubrew24 <sewelly@gmail.com>
2019-03-29 11:18:17 +0000

    delete button

--- client/post.js
+++ client/post.js
@@ -7,6 +7,8 @@ class Post {
         this.postImage = post.image
         this.username = post.user.f_name + " " + post.user.l_name
         this.image =  post.user.image || 'images/placeholder.jpg'
+        this.postId = post.id;
+        this.user = post.user.id;
 
         this.el = document.createElement("div")
         this.el.className = "card-body list-group-item"
@@ -16,6 +18,32 @@ class Post {
         } else {
             this.create();
         }
+        this.addDeleteListener()
+    }
+
+    addDeleteListener(){
+        if(this.el.querySelector(".delete-btn")){
+            this.el.querySelector(".delete-btn").addEventListener("click", () => {
+                this.el.remove();
+                API.deletePost(this.postId);
+            });
+        }
+    }
+
+    createDeleteBtn(){
+        let deleteBtn = document.createElement("button")
+        this.el.appendChild(deleteBtn)
+        deleteBtn.className = "delete-btn"
+        deleteBtn.innerText = "Remove"
+    }
+
+    deleteButton(){
+        if (document.cookie){
+            let userId = document.cookie.split('=')[2];
+            if (this.user == userId){
+                this.createDeleteBtn();
+            }
+        }
     }
 
     create() {
@@ -24,6 +52,7 @@ class Post {
         <p class="card-text">${this.username}</p>
         <p class="card-text">${this.comment}</p>
         <p class="card-text"><small class="text-muted">Posted at ${this.created_at}</small></p>`
+        this.deleteButton();
     }
 
     createWithImage(){
@@ -40,5 +69,8 @@ class Post {
                     <img src="${this.postImage}" class="card-img" >
                 </div>
             </div>`;
+        this.deleteButton();
     }
+
+
 }
